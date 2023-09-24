89c677435f90e05d197b0d2f72b7c5a778a3738b
riqthedev <riqthedev@gmail.com>
2023-09-24 04:46:54 -0400

    homepage added"
    "

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
-import Image from 'next/image'
-import {Card, CardHeader, CardTitle} from "@/components/ui/card"
-import { Button } from '@/components/ui/button'
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 export default function Home() {
   return (
-    <Card>
+    <Card className="w-[500px] h-[500px] mt-20 ml-20 ">
       <CardHeader>
         <CardTitle>ScoreBook</CardTitle>
+        <CardDescription>A Better Way Of Stat Tracking</CardDescription>
       </CardHeader>
-      <Button>Start</Button>
+      <CardContent>
+        <Button className="mt-4">Guest</Button>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Sign Up</Button>
+        <Button variant="outline">Login</Button>
+      </CardFooter>
     </Card>
   )
 }
