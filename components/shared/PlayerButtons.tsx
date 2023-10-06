47650d89824bbcd0f4ee821c2689ef26808201b4
riqthedev <riqthedev@gmail.com>
2023-10-06 16:16:34 -0700

    implemented player active

--- components/shared/PlayerButtons.tsx
+++ components/shared/PlayerButtons.tsx
@@ -1,19 +1,21 @@
 "use client"
 
 import { Button } from "../ui/button"
-import { Stat } from "../shared/PlayerRows" 
+import { Player, Stat } from "../shared/PlayerRows" 
 
 interface Props {
     updateStat: (stat: Stat) => void
+    player: Player
 }
 
 export default function PlayerButtons (props: Props) {
-    const updateStat = props.updateStat
+    const {updateStat, player} = props
+    
     return (
         <div className="player-buttons">
             <Button className="bg-red-500">+</Button>
             <Button>-</Button>
-            <Button onClick={() => updateStat("2P")}>Made 2PT</Button>
+            <Button onClick={() => updateStat("2P")} disabled={!player.active}>Made 2PT</Button>
             <Button>Miss 2PT</Button>
             <Button>Made 3PT</Button>
             <Button>Miss 3PT</Button>
--- components/shared/PlayerRows.tsx
+++ components/shared/PlayerRows.tsx
@@ -13,7 +13,7 @@ import PlayerButtons from "./PlayerButtons"
 
 export interface Player {
     id: number;
-    status: string;
+    active: boolean;
     playerName: string;
     points: number;
     rebounds: number;
@@ -22,33 +22,38 @@ export interface Player {
 }
 
 
-export type Stat = "rebounds" | "steals" | "FT" | "2P" | "3P" | "Miss 3P" | "Miss 2P" | "Miss FT" | "assist" | "block" | "foul"
+export type Stat = "rebound" | "steal" | "FT" | "2P" | "3P" | "Miss 3P" | "Miss 2P" | "Miss FT" | "assist" | "block" | "foul" | "sub"
 
 
 type Players = Player[]
 
 const initialPlayers: Players = [
-    { id: 1, status: "Active", playerName: "K.Durant", points: 29, rebounds: 4, fouls: 0, plus: 90},
-    { id: 2, status: "Active", playerName: "S,Curry", points: 40, rebounds: 2, fouls: 1, plus: 90},
-    { id: 3, status: "Active", playerName: "D.Green", points: 5, rebounds: 10, fouls: 4, plus: 90},
-    { id: 4, status: "Active", playerName: "J.Poole", points: 15, rebounds: 4, fouls: 1, plus: 90},
-    { id: 5, status: "Active", playerName: "K.Thompson", points: 10, rebounds: 4, fouls: 2, plus: 90},
-    { id: 6, status: "Active", playerName: "K.Looney", points: 2, rebounds: 18, fouls: 2, plus: 20 }
+    { id: 1, active: true, playerName: "K.Durant", points: 29, rebounds: 4, fouls: 0, plus: 90 },
+    { id: 2, active: true, playerName: "S,Curry", points: 40, rebounds: 2, fouls: 1, plus: 90 },
+    { id: 3, active: true, playerName: "D.Green", points: 5, rebounds: 10, fouls: 4, plus: 90 },
+    { id: 4, active: true, playerName: "J.Poole", points: 15, rebounds: 4, fouls: 1, plus: 90 },
+    { id: 5, active: true, playerName: "K.Thompson", points: 10, rebounds: 4, fouls: 2, plus: 90 },
+    { id: 6, active: false, playerName: "K.Looney", points: 2, rebounds: 18, fouls: 2, plus: 20 }
 ]
 const absurd = (x: never) => { }
 
-const updateStatGenerator = (players: Players, setPlayers: React.Dispatch<React.SetStateAction<Players>>, id: Number): (stat: Stat) => void  => {
+const updateStatGenerator = (players: Players, setPlayers: React.Dispatch<React.SetStateAction<Players>>, id: Number): (stat: Stat) => void => {
     const player = players.find((player) => player.id == id)
     return (stat: Stat): void => {
         if (!player) {
-            return 
+            return
         }
         switch (stat) {
             case "2P":
                 player.points += 2
-                players.forEach((teammate) => teammate.plus += 2)
+                players.filter((teammate) => teammate.active).forEach((teammate) => teammate.plus += 2)
                 setPlayers([...players])
-                return 
+                return
+
+            case "sub":
+                player.active = !player.active
+                setPlayers([...players])
+                return
             default:
                 absurd(stat)
                 return
@@ -60,10 +65,11 @@ const updateStatGenerator = (players: Players, setPlayers: React.Dispatch<React.
 }
 
 
+
 export default function PlayerRows() {
 
     const [players, setPlayers] = useState<Players>(initialPlayers)
-    
+
 
 
 
@@ -71,17 +77,19 @@ export default function PlayerRows() {
 
     return (
         <>
-            {players.map(player => (
-                <TableRow key={player.id}>
-                    <TableCell>{player.status}</TableCell>
+            {players.map(player => {
+                const updateStat = updateStatGenerator(players, setPlayers, player.id)
+                const cantSub = player.active || players.filter((teammate) => teammate.active).length < 5 
+                return (<TableRow key={player.id}>
+                    <TableCell><input type="checkbox" disabled={!cantSub} checked={player.active} onChange={() => updateStat("sub")} /></TableCell>
                     <TableCell>{player.playerName}</TableCell>
                     <TableCell>{player.points}</TableCell>
                     <TableCell>{player.rebounds}</TableCell>
                     <TableCell>{player.fouls}</TableCell>
                     <TableCell>{player.plus}</TableCell>
-                    <TableCell> <PlayerButtons updateStat={updateStatGenerator(players, setPlayers, player.id)}/> </TableCell>
-                </TableRow>
-            ))}
+                    <TableCell> <PlayerButtons player={player} updateStat={updateStat} /> </TableCell>
+                </TableRow>)
+            })}
         </>
     )
 }
