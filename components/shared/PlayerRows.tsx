401bb9d94352d2bb1fc88885a2aa5a279e1621c2
riqthedev <riqthedev@gmail.com>
2023-10-06 15:43:02 -0700

    implement plus

--- components/shared/PlayerRows.tsx
+++ components/shared/PlayerRows.tsx
@@ -18,6 +18,7 @@ export interface Player {
     points: number;
     rebounds: number;
     fouls: number;
+    plus: number;
 }
 
 
@@ -27,12 +28,12 @@ export type Stat = "rebounds" | "steals" | "FT" | "2P" | "3P" | "Miss 3P" | "Mis
 type Players = Player[]
 
 const initialPlayers: Players = [
-    { id: 1, status: "Active", playerName: "K.Durant", points: 29, rebounds: 4, fouls: 0 },
-    { id: 2, status: "Active", playerName: "S,Curry", points: 40, rebounds: 2, fouls: 1 },
-    { id: 3, status: "Active", playerName: "D.Green", points: 5, rebounds: 10, fouls: 4 },
-    { id: 4, status: "Active", playerName: "J.Poole", points: 15, rebounds: 4, fouls: 1 },
-    { id: 5, status: "Active", playerName: "K.Thompson", points: 10, rebounds: 4, fouls: 2 },
-    { id: 6, status: "Active", playerName: "K.Looney", points: 2, rebounds: 18, fouls: 2 }
+    { id: 1, status: "Active", playerName: "K.Durant", points: 29, rebounds: 4, fouls: 0, plus: 90},
+    { id: 2, status: "Active", playerName: "S,Curry", points: 40, rebounds: 2, fouls: 1, plus: 90},
+    { id: 3, status: "Active", playerName: "D.Green", points: 5, rebounds: 10, fouls: 4, plus: 90},
+    { id: 4, status: "Active", playerName: "J.Poole", points: 15, rebounds: 4, fouls: 1, plus: 90},
+    { id: 5, status: "Active", playerName: "K.Thompson", points: 10, rebounds: 4, fouls: 2, plus: 90},
+    { id: 6, status: "Active", playerName: "K.Looney", points: 2, rebounds: 18, fouls: 2, plus: 20 }
 ]
 const absurd = (x: never) => { }
 
@@ -45,6 +46,7 @@ const updateStatGenerator = (players: Players, setPlayers: React.Dispatch<React.
         switch (stat) {
             case "2P":
                 player.points += 2
+                players.forEach((teammate) => teammate.plus += 2)
                 setPlayers([...players])
                 return 
             default:
@@ -76,6 +78,7 @@ export default function PlayerRows() {
                     <TableCell>{player.points}</TableCell>
                     <TableCell>{player.rebounds}</TableCell>
                     <TableCell>{player.fouls}</TableCell>
+                    <TableCell>{player.plus}</TableCell>
                     <TableCell> <PlayerButtons updateStat={updateStatGenerator(players, setPlayers, player.id)}/> </TableCell>
                 </TableRow>
             ))}
